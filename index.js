848aee09ea6ef1d445127af7751da10969efb1f1
Giancarlo Whitaker <giancarlo.whitaker@gmail.com>
2023-08-09 19:41:14 -0700

    Add code to index.js to output a question to a file

--- index.js
+++ index.js
@@ -1,13 +1,30 @@
 // TODO: Include packages needed for this application
+const inquirer = require('inquirer');
+const fs = require('fs');
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+    {
+        type: 'input',
+        message: 'What is the name of your project?',
+        name: 'projname',
+    }
+];
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(fileName, data) {
+    fs.writeFile(fileName, JSON.stringify(data), function(error) {
+        error ? console.log(error) : console.log("Success!");
+    });
+}
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() {
+    inquirer.prompt(questions).then((response) => {
+        console.log(response);
+        writeToFile("testFile.json", response);
+    });
+}
 
 // Function call to initialize app
 init();
