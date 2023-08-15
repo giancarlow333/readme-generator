88a0bb4c37dc9b2acbc7d41d79aba36de1ed143d
Giancarlo Whitaker <giancarlo.whitaker@gmail.com>
2023-08-14 21:29:50 -0700

    Add skeleton for license functions

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,14 +1,71 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+function renderLicenseBadge(license) {
+  switch (license) {
+    case "GNU GPLv3":
+      break;
+    case "GNU LGPLv3":
+      break;
+    case "Mozilla Public License 2.0":
+      break;
+    case "Apache License 2.0":
+      break;
+    case "MIT License":
+      break;
+    case "Boost Software License 1.0":
+      break;
+    case "The Unlicense":
+      break;
+    default:
+      break;
+  }
+}
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
-function renderLicenseLink(license) {}
+function renderLicenseLink(license) {
+  switch (license) {
+    case "GNU GPLv3":
+      break;
+    case "GNU LGPLv3":
+      break;
+    case "Mozilla Public License 2.0":
+      break;
+    case "Apache License 2.0":
+      break;
+    case "MIT License":
+      break;
+    case "Boost Software License 1.0":
+      break;
+    case "The Unlicense":
+      break;
+    default:
+      break;
+  }
+}
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
-function renderLicenseSection(license) {}
+function renderLicenseSection(license) {
+  switch (license) {
+    case "GNU GPLv3":
+      break;
+    case "GNU LGPLv3":
+      break;
+    case "Mozilla Public License 2.0":
+      break;
+    case "Apache License 2.0":
+      break;
+    case "MIT License":
+      break;
+    case "Boost Software License 1.0":
+      break;
+    case "The Unlicense":
+      break;
+    default:
+      break;
+  }
+}
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
