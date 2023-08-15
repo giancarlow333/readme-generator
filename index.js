2a2662a5898f4d94acc3bf2501e50a3f67fe0933
Giancarlo Whitaker <giancarlo.whitaker@gmail.com>
2023-08-14 17:16:41 -0700

    Add user questions (via inquierer)

--- index.js
+++ index.js
@@ -9,6 +9,47 @@ const questions = [
         type: 'input',
         message: 'What is the name of your project?',
         name: 'projname',
+    },
+    {
+        type: 'input',
+        message: 'How would you describe your project?',
+        name: 'projdesc',
+    },
+    {
+        type: 'input',
+        message: 'How does one install your project?',
+        name: 'projinstall',
+    },
+    {
+        type: 'input',
+        message: 'How does one use your project?',
+        name: 'projusage',
+    },
+    {
+        type: 'input',
+        message: 'How does one contribute to your project?',
+        name: 'projcontrib',
+    },
+    {
+        type: 'input',
+        message: 'How does one test your project?',
+        name: 'projtest',
+    },
+    {
+      type: 'list',
+      message: 'How is your project licensed?',
+      name: 'projlicense',
+      choices: ["GNU GPLv3", "GNU LGPLv3", "Mozilla Public License 2.0", "Apache License 2.0", "MIT License", "Boost Software License 1.0", "The Unlicense", "No license"]
+    },
+    {
+        type: 'input',
+        message: 'What is your GitHub username?',
+        name: 'username',
+    },
+    {
+        type: 'input',
+        message: 'What is your email?',
+        name: 'useremail',
     }
 ];
 
